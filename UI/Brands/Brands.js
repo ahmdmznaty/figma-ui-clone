1052e8e476bef3570a2c45a0e8ab5ed751471237
Ahmed M. Znaty <ahmdmznaty@gmail.com>
2023-04-14 11:11:12 +0200

    Brands

--- UI/Brands/Brands.js
+++ UI/Brands/Brands.js
@@ -2,8 +2,19 @@ import comket from "../dependencies/comket.js"
 
 export default function Brands() {
     let parent = comket.div({
-        text: "Brands"
+        class: "brands",
+        children: [
+            comket.p({
+                children: [
+                    comket.mark({text: "250+"}),
+                    comket.span({text: "Collaboration"})
+                ]
+            }),
+            comket.img({src: "./media/brands/duolingo.svg"}),
+            comket.img({src: "./media/brands/codecov.svg"}),
+            comket.img({src: "./media/brands/usertesting.svg"}),
+            comket.img({src: "./media/brands/magicleap.svg"}),
+        ]
     })
-    // working here...
     return parent
 }
